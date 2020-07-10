a54486f7913669b1837dc63b230de502dcba240e
minhhv <minhhoangvan@admicro.vn>
2020-07-10 17:25:20 +0700

    + sorting algorithms

---
+++ src/main/python/sortalgorithms/AbstractSorting.py
@@ -0,0 +1,7 @@
+from typing import List
+from abc import ABC
+
+
+class AbstractSorting(ABC):
+    def sort(self, nums: List) -> List:
+        pass
---
+++ src/main/python/sortalgorithms/Merging.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from . import AbstractSorting
+
+
+class Merging(AbstractSorting):
+    def sort(self, nums: List) -> List:
+        return nums
