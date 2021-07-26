061208f92f95db45fa39d1d9bb3b5833c519c3fd
kumin242 <minh.hoang4@tiki.vn>
2021-07-26 11:21:51 +0700

    weekly contest

---
+++ src/main/python/weekendcontest/25-07-2021/MaximumNumber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        return 0
+
+if __name__ == '__main__':
+    sol = Solution()
+    print()
